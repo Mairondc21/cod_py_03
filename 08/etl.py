66fe136f0a6136b93a7aa4c4d327efc5062f349f
Mairondc21 <duartemairon@gmail.com>
2024-09-03 17:01:06 -0300

    20240903H17

---
+++ 08/etl.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os 
+import glob
+
+def extrair_dados(pasta: str) -> pd.DataFrame:
+    arquivos_json = glob.glob(os.path.join(pasta,'*.json'))
+    df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
+    df_total = pd.concat(df_list, ignore_index=True)
+
+    return df_total
+
+def calcular_kpi_de_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
+    df["Total"] = df["Quantidade"] * df["Venda"]
+    return df
+
+def carregar_dados(df:pd.DataFrame, formato_saida: list) -> pd.DataFrame:
+    for formato in  formato_saida:
+        if formato == 'csv':
+            df.to_csv("df_tratado.csv",index=False)
+        if formato == 'parquet':
+            df.to_parquet("df_tratado.parquet")
+
+def pipeline_leitura_vendas(pasta:str, formato_saida:list ):
+    df_extraido = extrair_dados(pasta)
+    df_novo = calcular_kpi_de_total_de_vendas(df_extraido)
+    carregar_dados(df_novo,formato_saida)
+
+if __name__ == "__main__":
+    pasta = 'data'
+    formato_saida = ["csv","parquet"]
+    pipeline_leitura_vendas(pasta,formato_saida)
